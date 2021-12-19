e5b4d8a513581f516cc574cbc4fb547600ac3001
ot0m1 <coding_addiction@yahoo.co.jp>
2021-12-19 12:47:04 +0900

    add application files

---
+++ js/main.js
@@ -0,0 +1,44 @@
+const vm = Vue.createApp({
+  data () {
+    return {
+      todos: [],
+      newTodo: '',
+      editedTodo: '',
+      MessageIfNone: 'Notnig To do.'
+    }
+  },
+  watch: {
+    todos: {
+      handler: function () {
+        localStorage.setItem('todos', JSON.stringify(this.todos))
+      },
+      deep: true
+    }
+  },
+  mounted: function () {
+    this.todos = JSON.parse(localStorage.getItem('todos')) || []
+  },
+  methods: {
+    addItem () {
+      const item = { title: this.newTodo, isDone: false , editable: false}
+      this.todos.push(item)
+      this.newTodo = ''
+    },
+    startEditing (index) {
+      this.todos[index].editable = true 
+    },
+    editItem (index) {
+      console.log(this.editedTodo)
+      this.todos[index].title = this.editedTodo
+      this.todos[index].editable = false
+    },
+    cancelEditing (index) {
+      this.todos[index].editable = false
+    },
+    deleteItem  (index) {
+      if (confirm('Are you sure?')) {
+        this.todos.splice(index, 1)
+      }
+    }
+  }
+}).mount('#app')
